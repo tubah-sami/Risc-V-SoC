f780705704ad64165e748464a8bd3d9c18303788
tubah-sami <tubah17623@gmail.com>
2026-09-07 23:04:15 +0500

    Delete Adder.v

--- Adder.v
+++
@@ -1,9 +0,0 @@
-`include "RISCV_PKG.vh"
-module Adder(
-    input [`INSTRUCTION_SIZE-1:0] in1,
-    input [`INSTRUCTION_SIZE-1:0] in2,
-    output [`INSTRUCTION_SIZE-1:0] out
-);
-    assign out = in1 + in2;
-
-endmodule
